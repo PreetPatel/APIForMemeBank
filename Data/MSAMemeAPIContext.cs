70773e9b37ca803eacbd0a5f60d1064c0f707133
Preet Patel <iam@preetpatel.com>
2018-11-18 00:17:41 +1300

    Ïnitial Upload

---
+++ Data/MSAMemeAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MSAMemeAPI.Models
+{
+    public class MSAMemeAPIContext : DbContext
+    {
+        public MSAMemeAPIContext (DbContextOptions<MSAMemeAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MSAMemeAPI.Models.MemeItem> MemeItem { get; set; }
+    }
+}
